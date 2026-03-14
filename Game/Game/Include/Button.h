7aaa545de4d6be8564f04743966250a676b37514
Sean Lee <seanleehongwei@gmail.com>
2026-03-14 20:08:06 +0800

    Add new buttons and JSON data for level buttons
    New button class in Button.cpp and Button.h
     - NewButton class
    
    Json data for restart and quit buttons in the level added into a json
    file

---
+++ Game/Game/Include/Button.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <string>
+
+#include <AEEngine.h>
+#include <json/json.h>
+
+#include "Components.h"
+
+struct TextData {
+    std::string content_;
+    float x_{0.f}, y_{0.f};
+    float scale_{1.f};
+    float r_{1.f}, g_{1.f}, b_{1.f}, a_{1.f};
+};
+
+class NewButton {
+public:
+    void loadTexture(const char* path);
+    void loadMesh();
+    void initFromJson(const Json::Value& jsonBtn);
+    void updateTransform();
+    void draw(s8 font);
+    void unload();
+    bool checkMouseClick() const;
+
+private:
+    Transform transform_;
+    Graphics graphics_;
+    TextData text_;
+};
---
+++ Game/Game/Source/Button.cpp
@@ -0,0 +1,97 @@
+#pragma once
+
+#include "Button.h"
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include <AEEngine.h>
+#include <json/json.h>
+
+#include "Components.h"
+#include "Utils.h"
+
+void NewButton::loadTexture(const char* path) { graphics_.texture_ = AEGfxTextureLoad(path); }
+
+void NewButton::loadMesh() { graphics_.mesh_ = CreateRectMesh(); }
+
+// Usage example:
+// obj.initFromJson(root["buttons"]["Restart"]);
+void NewButton::initFromJson(const Json::Value& jsonBtn) {
+    transform_.pos_.x = jsonBtn["position"]["x"].asFloat();
+    transform_.pos_.y = jsonBtn["position"]["y"].asFloat();
+    transform_.scale_.x = jsonBtn["scale"]["x"].asFloat();
+    transform_.scale_.y = jsonBtn["scale"]["y"].asFloat();
+
+    const Json::Value& t = jsonBtn["text"];
+    text_.content_ = t["content"].asString();
+    text_.x_ = t["x"].asFloat();
+    text_.y_ = t["y"].asFloat();
+    text_.scale_ = t["scale"].asFloat();
+    text_.r_ = t["red"].asFloat();
+    text_.g_ = t["green"].asFloat();
+    text_.b_ = t["blue"].asFloat();
+    text_.a_ = t["alpha"].asFloat();
+}
+
+void NewButton::updateTransform() {
+    AEMtx33 scaleMtx, rotMtx, transMtx;
+    AEMtx33Scale(&scaleMtx, transform_.scale_.x, transform_.scale_.y);
+    AEMtx33Rot(&rotMtx, transform_.rotationRad_);
+    AEMtx33Trans(&transMtx, transform_.pos_.x, transform_.pos_.y);
+    AEMtx33Concat(&transform_.worldMtx_, &rotMtx, &scaleMtx);
+    AEMtx33Concat(&transform_.worldMtx_, &transMtx, &transform_.worldMtx_);
+}
+
+void NewButton::draw(s8 font) {
+    // Render button (no text)
+    AEGfxSetRenderMode(AE_GFX_RM_TEXTURE);
+    AEGfxSetBlendMode(AE_GFX_BM_BLEND);
+    AEGfxSetTransparency(1.0f);
+    AEGfxSetColorToMultiply(1.0f, 1.0f, 1.0f, 1.0f);
+    AEGfxSetTransform(transform_.worldMtx_.m);
+    AEGfxTextureSet(graphics_.texture_, 0.0f, 0.0f);
+    AEGfxMeshDraw(graphics_.mesh_, AE_GFX_MDM_TRIANGLES);
+
+    // Render text
+    AEGfxSetRenderMode(AE_GFX_RM_COLOR);
+    AEGfxPrint(font, text_.content_.c_str(), text_.x_, text_.y_, text_.scale_, text_.r_, text_.g_,
+               text_.b_, text_.a_);
+}
+
+void NewButton::unload() {
+    if (graphics_.texture_ != nullptr) {
+        AEGfxTextureUnload(graphics_.texture_);
+        graphics_.texture_ = nullptr;
+    }
+    if (graphics_.mesh_ != nullptr) {
+        AEGfxMeshFree(graphics_.mesh_);
+        graphics_.mesh_ = nullptr;
+    }
+}
+
+bool NewButton::checkMouseClick() const {
+    // Get mouse position
+    s32 mouseX{0};
+    s32 mouseY{0};
+
+    AEInputGetCursorPosition(&mouseX, &mouseY);
+    mouseX -= AEGfxGetWindowWidth() / 2;
+    mouseY = (AEGfxGetWindowHeight() / 2) - mouseY;
+
+    // Check if left mouse click
+    if (AEInputCheckReleased(AEVK_LBUTTON) || 0 == AESysDoesWindowExist()) {
+        // Check by checking if mouse pos falls within the button's collider box
+        f32 rect_half_width{transform_.scale_.x / 2.0f};
+        f32 rect_half_height{transform_.scale_.y / 2.0f};
+        if (mouseX >= (transform_.pos_.x - rect_half_width) &&
+            mouseX <= (transform_.pos_.x + rect_half_width) &&
+            mouseY >= (transform_.pos_.y - rect_half_height) &&
+            mouseY <= (transform_.pos_.y + rect_half_height)) {
+            return true;
+        }
+    }
+
+    return false;
+}
